09db36261cbc62bd7cf34c9272881aca54668e15
Lebroonx <147380255+Lebroonx@users.noreply.github.com>
2023-10-12 16:02:05 +0200

    Create functions.h

---
+++ headers/functions.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void await_input();
+void menu();
+void add_book();
+void search_book();
+void delete_book();
+void list_book();
+void exit_app();
+
+#endif
