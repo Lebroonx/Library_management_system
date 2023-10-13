ace5c44d9e326c56bc52aa39671ffe211b47049a
Lebroonx <147380255+Lebroonx@users.noreply.github.com>
2023-10-13 14:06:48 +0200

    Update functions.c

--- sources/functions.c
+++ sources/functions.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 struct book {
     int id;
-    char name[30];
-    char author[20];
+    char name[50];
+    char author[50];
 }
 bk[100] = { 0 };
-char searchname[50];
+char searchdata[50];
 
 void await_input()
 {
@@ -18,7 +19,7 @@ void await_input()
 
 void menu()
 {
-    printf("Library management system\n\n1. Ajouter un livre\n2. Rechercher un livre\n3. Supprimer un livre\n4. Afficher tous les livres\n5. Sortir\n\nQue voulez vous faire : ");
+    printf("\nLibrary management system\n\n1. Ajouter un livre\n2. Rechercher un livre\n3. Rechercher les livres d'un auteur\n4. Modifier un livre\n5. Supprimer un livre\n6. Afficher tous les livres\n7. Sortir\n\nQue voulez vous faire : ");
 }
 
 void add_book()
@@ -41,12 +42,45 @@ void add_book()
 void search_book()
 {
     printf("\nEntrer le nom du livre: ");
-    scanf("%s", &searchname);
+    scanf("%s", &searchdata);
+    printf("\n\nID\tNom\t\tAuteur\n");
     for (int i = 1; i < 20; i++)
     {
-        if (strcmp(searchname, bk[i].name) == 0)
+        if (strcmp(searchdata, bk[i].name) == 0)
         {
-            printf("\nInformations du livre\n\nID du livre: %d\nNom du livre: %s\nAuteur: %s\n", bk[i].id, bk[i].name, bk[i].author);
+            printf("%d\t%s\t\t%s\n", bk[i].id, bk[i].name, bk[i].author);
+        }
+    }
+}
+
+void search_author()
+{
+    printf("\nEntrer le nom de l'auteur: ");
+    scanf("%s", &searchdata);
+    printf("\n\nID\tNom\t\tAuteur\n");
+    for (int i = 1; i < 20; i++)
+    {
+        if (strcmp(searchdata, bk[i].author) == 0)
+        {
+            printf("%d\t%s\t\t%s\n", bk[i].id, bk[i].name, bk[i].author);
+        }
+    }
+}
+
+void modify_book()
+{
+    printf("\nEntrer le nom du livre: ");
+    scanf("%s", &searchdata);
+    printf("\n\nID\tNom\t\tAuteur\n");
+    for (int i = 1; i < 100; i++)
+    {
+        if (strcmp(searchdata, bk[i].name) == 0)
+        {
+            printf("%d\t%s\t\t%s\n", bk[i].id, bk[i].name, bk[i].author);
+            printf("\nEntrer le nom modifié du livre: ");
+            scanf("%s", bk[i].name);
+            printf("\nEntrer l'auteur modifié du livre: ");
+            scanf("%s", bk[i].author);
         }
     }
 }
@@ -54,11 +88,13 @@ void search_book()
 void delete_book()
 {
     printf("\nEntrer le nom du livre: ");
-    scanf("%s", &searchname);
+    scanf("%s", &searchdata);
+    printf("\n\nID\tNom\t\tAuteur\n");
     for (int i = 1; i < 100; i++)
     {
-        if (strcmp(searchname, bk[i].name) == 0)
+        if (strcmp(searchdata, bk[i].name) == 0)
         {
+            printf("%d\t%s\t\t%s\n", bk[i].id, bk[i].name, bk[i].author);
             bk[i].id = 0;
         }
     }
@@ -78,6 +114,6 @@ void list_book()
 
 void exit_app()
 {
-    printf("\n\t\tAu revoir");
+    printf("\nAu revoir");
     exit(0);
 }
