26700989ad32895adc887b83ee84de28f3c2876e
Lebroonx <147380255+Lebroonx@users.noreply.github.com>
2023-10-13 14:07:10 +0200

    Update functions.h

--- headers/functions.h
+++ headers/functions.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
@@ -5,6 +6,8 @@ void await_input();
 void menu();
 void add_book();
 void search_book();
+void search_author();
+void modify_book();
 void delete_book();
 void list_book();
 void exit_app();
